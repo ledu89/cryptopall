48da7c7412a6bc9b793d8404f5e789b54c7a6146
ledu89 <dninkov89@gmail.com>
2024-10-30 14:40:42 +0100

    cors

--- strapi/my-project/config/middlewares.js
+++ strapi/my-project/config/middlewares.js
@@ -1,49 +1,8 @@
-// (module.exports = [
-//   "strapi::logger",
-//   "strapi::errors",
-//   "strapi::security",
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ]),
-//   {
-//     name: "strapi::security",
-//     config: {
-//       cors: {
-//         origin: ["https://strapi-cryptopall.onrender.com"],
-//       },
-//     },
-//   };
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "https://strapi-cryptopall.onrender.com", // Backend URL
-        "https://cryptopall-git-main-ledus-projects.vercel.app", // Frontend URL
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      headers: ["Content-Type", "Authorization"],
-      keepHeadersOnError: true,
-    },
-  },
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          "script-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:"],
-        },
-      },
-    },
-  },
+  "strapi::security",
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
