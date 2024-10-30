344e2d097349eb852b15425730ee666e3ac4b1e1
ledu89 <dninkov89@gmail.com>
2024-10-30 15:15:02 +0100

    cors

--- strapi/my-project/config/middlewares.js
+++ strapi/my-project/config/middlewares.js
@@ -1,8 +1,25 @@
+// module.exports = [
+//   "strapi::logger",
+//   "strapi::errors",
+//   "strapi::security",
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
 module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: "*", // Allow all origins (for development/testing only)
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
